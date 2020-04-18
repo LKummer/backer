eda7f6aa356e54682cfacf6eebbcfa6daba5dc77
Lior Kummer <LKummer41@gmail.com>
2020-04-18 22:09:57 +0300

    Add docstring to the not yet implemented method.

--- backer/archive.py
+++ backer/archive.py
@@ -2,4 +2,12 @@
 """
 
 def archive_files(files, root, output):
-    print('archive_files called with {}, {}, {}.'.format(files, root, output))
+    """Archive files into a ZIP archive.
+    
+    Args:
+        files (:obj:`list` of :obj:`pathlib.Path`): List of paths to add to the
+            archive.
+        root (pathlib.Path): Root of the content added to the archive.
+        output (pathlib.Path): Output archive file.
+    """
+    pass
