46558ee35a510e26ba469a891a5775a74c36b639
Lior Kummer <LKummer41@gmail.com>
2020-04-21 23:14:57 +0300

    Add more tests for the archive module.

--- tests/test_archive.py
+++ tests/test_archive.py
@@ -4,7 +4,7 @@
 from pathlib import Path
 from zipfile import Path as ZipPath
 
-from pytest import fixture
+from pytest import fixture, raises
 
 from backer.archive import archive_files
 
@@ -16,6 +16,7 @@ def data_path():
 
 class TestFileArchiving:
     def test_multiple_files(self, data_path, tmp_path):
+        """Files are added correctly."""
         files = [
             data_path.joinpath("files/file_a.md"),
             data_path.joinpath("files/file_b.md"),
@@ -35,6 +36,7 @@ class TestFileArchiving:
             assert archive.joinpath("file_c.md").exists()
 
     def test_folder(self, data_path, tmp_path):
+        """Folder paths are added correctly (recursively)"""
         files = [data_path.joinpath("files")]
         result = tmp_path.joinpath("folder.zip")
         archive_files(files, data_path, result)
@@ -48,6 +50,7 @@ class TestFileArchiving:
             assert archive.joinpath("files/file_c.md").exists()
 
     def test_nested_files(self, data_path, tmp_path):
+        """Nested folders are added correctly."""
         files = [data_path.joinpath("nested/some/nested/folder/nested.md")]
         root = data_path.joinpath("nested")
         result = tmp_path.joinpath("nested.zip")
@@ -58,3 +61,36 @@ class TestFileArchiving:
             archive = ZipPath(result)
             # Check that the file exists inside the archive.
             assert archive.joinpath("some/nested/folder/nested.md").exists()
+
+    def test_no_files(self, data_path, tmp_path):
+        """Archive is not created when no files are given."""
+        result = tmp_path.joinpath("empty.zip")
+        archive_files([], data_path, result)
+        assert not result.exists()
+
+    def test_files_do_not_exist(self, data_path, tmp_path):
+        """Files that do not exist are not added to the archive silently."""
+        files = [
+            data_path.joinpath("files/file_a.md"),
+            data_path.joinpath("files/some_file_that_does_not_exist.md"),
+        ]
+        result = tmp_path.joinpath("do_not_exist.zip")
+        archive_files(files, data_path, result)
+        # Check to avoid errors when the zip file does not exist.
+        assert result.exists()
+        if result.exists():
+            archive = ZipPath(result)
+            assert archive.joinpath("files/file_a.md").exists()
+            # Check that the missing file is not inside the archive.
+            assert not archive.joinpath(
+                "files/some_file_that_does_not_exist.md"
+            ).exists()
+
+    def test_root_is_not_in_file_paths(self, data_path, tmp_path):
+        """Exception is raised when file paths cannot be made relative to the root path."""
+        files = [
+            data_path.joinpath("files/file_a.md"),
+        ]
+        result = tmp_path.joinpath("invalid_root.zip")
+        with raises(ValueError):
+            archive_files(files, data_path.joinpath('../../'), result)
