da92159b386945a9b167d2786bdbf1ff01c81b3f
Lior Kummer <LKummer41@gmail.com>
2020-04-21 13:21:01 +0300

    Configure the CLI script with Poetry.

--- backer/main.py
+++ backer/main.py
@@ -1,6 +1,9 @@
 """Main CLI Entry Point."""
 from backer.cli import get_parser
 
+def run():
+    args = get_parser().parse_args()
+    print(args)
+
 if __name__ == "__main__":
-    ARGS = get_parser().parse_args()
-    print(ARGS)
+    run()
