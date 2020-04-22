5c39a6ba0d5554713c4bee8d2ace76d95cd3caa4
Lior Kummer <LKummer41@gmail.com>
2020-04-22 03:02:33 +0300

    Refactor main run function into the backer module.

--- backer/__init__.py
+++ backer/__init__.py
@@ -1,2 +1,12 @@
 """Archiving Command Line App."""
+
+from backer.cli import get_parser
+
+
 __version__ = "0.1.0"
+
+
+def run():
+    """Run the CLI program."""
+    args = get_parser().parse_args()
+    print(args)
