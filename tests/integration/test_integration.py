d741dc9fa34b6d12273450bf2c4e8b464862b36e
Lior Kummer <LKummer41@gmail.com>
2020-04-25 21:50:59 +0300

    Improve the first run integration test.

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -14,6 +14,12 @@ class TestIntegration:
             f'poetry run backer tests/data/files -o "{tmp_path}" -r tests/data/files'
         )
         assert result == 0
+        # Check the created configuration.
+        config = tmp_path.joinpath(".backerrc")
+        assert config.exists()
+        config_text = config.open("r").read()
+        assert config_text == '{"count": 3}'
+        # Check the created archive.
         archives = list(tmp_path.glob("*.zip"))
         assert len(archives) == 1
         if archives:
